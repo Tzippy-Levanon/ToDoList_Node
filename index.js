fe41e28a1c4b10bf327b26b540cb493bbb4d5dd3
Tzippy <tzippy876@gmail.com>
2025-03-11 20:50:09 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import renderApi from '@api/render-api';
+import express from "express";
+import dotenv from 'dotenv'
+import cors from 'cors'
+import bodyParser from "body-parser";
+
+const app = express()
+const port =process.env.PORT || 3001
+dotenv.config()
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('', (req, res) => {
+  renderApi.auth(process.env.RENDER_API_KEY);
+  renderApi.listServices({ includePreviews: 'true', limit: '20' })
+    .then( data  => {return res.status(200).send({data: data})})
+    .catch(err => console.error(err));
+})
+
+app.listen(port, () => {
+  console.log(`my application is listening on http://localhost:${port}`);
+})
